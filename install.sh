3a6bcc289a8bafa8c6992caaa9962fb289e41fe1
song@lx <chaosstar3@gmail.com>
2018-08-28 21:53:58 +0900

    add git

--- install.sh
+++ install.sh
@@ -3,12 +3,23 @@ home_cfg=(tmux.conf vimrc)
 
 pushd . > /dev/null
 cd $(dirname "$0")
-dotdir=$(dirs -p | sed -e 's/^~\///;q')
+dotdir=$(pwd)
+dotdir_r=$(dirs -p | sed -e 's/^~\///;q')
 
 # symlink to homedir with .
 for cfg in ${home_cfg[@]}; do
-	ln -s -i $dotdir/$cfg ~/.$cfg
+	ln -s -i $dotdir_r/$cfg ~/.$cfg
 done
+
+# git config
+if (command -v git > /dev/null); then
+	exe git config --global include.path $dotdir_r/gitconfig
+	exe git config --global core.excludesFile $dotdir/gitignore.global
+
+	if (command -v vim > /dev/null); then
+		p git config --global core.editor vim
+	fi
+fi
 
 # return to life
 popd > /dev/null
