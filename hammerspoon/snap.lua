3eec904a8de34a285f96547c29ca47b1403315c5
song@mm <chaosstar3@gmail.com>
2025-10-01 04:38:17 +0900

    hammerspoon: improve

--- hammerspoon/snap.lua
+++ hammerspoon/snap.lua
@@ -1,12 +1,13 @@
--- copy to ~/.hammerspoon/init.lua
--- snap app
-function task(path, arg)
+-- Module: Snap App. Hotkey
+task = { apps={} }
+
+function task.task(path, arg)
 	return function()
 		hs.task.new(path, nil, function() return false end, arg):start()
 	end
 end
-function regist(modifier, table)
-	for i, apps in pairs(table) do
+function task.regist(modifier, table)
+	for i, id in pairs(table) do
 		local app = apps[id]
 		if app then
 			if type(app) == "string" then
@@ -20,31 +21,26 @@ function regist(modifier, table)
 	end
 end
 
-apps = {
-	dir="finder",
-	hello=task("/bin/echo", {os.getenv("HOME") .. "HELLO"})
-}
-regist("option", {"term", "dir", "web", "editor", "git"})
-regist("option+shift", {[3]="web_alt", [4]="md"})
-
--- snap window
-ratio_conf = {50, 66, 90, 33}
-
+-- Module: Snap Window
+window = {}
 M = {MOVE=1, RESIZE=2}
 D = {UP=1, DOWN=2, LEFT=3, RIGHT=4, CENTER=5}
 W = {WIDTH=1, HEIGHT=2}
+window.ratio_conf = {50, 66, 90, 33}
+window.M, window.D = M, D
+
 prev_m = nil
 prev_d = nil
 move = 0
 
-function win_maximize()
+function window.maximize()
 	local win = hs.window.focusedWindow()
 	local f = win:frame()
 	local s = win:screen():frame()
 	win:setFrame(s)
 end
 
-function win_move(method, direction)
+function window.move(method, direction)
 	local win = hs.window.focusedWindow()
 	local f = win:frame()
 	local s = win:screen():frame()
@@ -81,7 +77,7 @@ function win_move(method, direction)
 		move = 0
 	end
 
-	local ratio = ratio_conf[move % #ratio_conf + 1]/100
+	local ratio = window.ratio_conf[move % #window.ratio_conf + 1]/100
 	--print("ratio: ", ratio)
 
 	if way == W.WIDTH then
@@ -108,7 +104,7 @@ function win_move(method, direction)
 	win:setFrame(f)
 end
 
-function win_move_monitor(direction)
+function window.move_monitor(direction)
 	local win = hs.window.focusedWindow()
 	local m = nil
 	if direction > 0 then
@@ -122,19 +118,23 @@ function win_move_monitor(direction)
 	end
 end
 
+function window.regist()
+	hs.hotkey.bind("option", "left", function() window.move(M.MOVE, D.LEFT) end)
+	hs.hotkey.bind("option", "right", function() window.move(M.MOVE, D.RIGHT) end)
+	hs.hotkey.bind("option", "up", function() window.move(M.MOVE, D.UP) end)
+	hs.hotkey.bind("option", "down", function() window.move(M.MOVE, D.DOWN) end)
+	hs.hotkey.bind({"option", "cmd"}, "left", function() window.move(M.RESIZE, D.LEFT) end)
+	hs.hotkey.bind({"option", "cmd"}, "right", function() window.move(M.RESIZE, D.RIGHT) end)
+	hs.hotkey.bind({"option", "cmd"}, "up", function() window.move(M.RESIZE, D.UP) end)
+	hs.hotkey.bind({"option", "cmd"}, "down", function() window.move(M.RESIZE, D.DOWN) end)
+	hs.hotkey.bind({"option", "shift"}, "left", function() window.move_monitor(-1) end)
+	hs.hotkey.bind({"option", "shift"}, "right", function() window.move_monitor(1) end)
+	hs.hotkey.bind({"option", "shift"}, "up", window.maximize)
+	hs.hotkey.bind({"option", "shift"}, "down", function() window.move(M.MOVE, D.CENTER) end)
+end
 
-
-
-hs.hotkey.bind("option", "left", function() win_move(M.MOVE, D.LEFT) end)
-hs.hotkey.bind("option", "right", function() win_move(M.MOVE, D.RIGHT) end)
-hs.hotkey.bind("option", "up", function() win_move(M.MOVE, D.UP) end)
-hs.hotkey.bind("option", "down", function() win_move(M.MOVE, D.DOWN) end)
-hs.hotkey.bind({"option", "cmd"}, "left", function() win_move(M.RESIZE, D.LEFT) end)
-hs.hotkey.bind({"option", "cmd"}, "right", function() win_move(M.RESIZE, D.RIGHT) end)
-hs.hotkey.bind({"option", "cmd"}, "up", function() win_move(M.RESIZE, D.UP) end)
-hs.hotkey.bind({"option", "cmd"}, "down", function() win_move(M.RESIZE, D.DOWN) end)
-hs.hotkey.bind({"option", "shift"}, "left", function() win_move_monitor(-1) end)
-hs.hotkey.bind({"option", "shift"}, "right", function() win_move_monitor(1) end)
-hs.hotkey.bind({"option", "shift"}, "up", win_maximize)
-hs.hotkey.bind({"option", "shift"}, "down", function() win_move(M.MOVE, D.CENTER) end)
-
+window.regist() -- regist default
+return {
+	task=task,
+	window=window,
+}
