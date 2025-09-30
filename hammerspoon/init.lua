0eec1c07e4adb5d4e368ed933d45786298c79ef0
song@mm <chaosstar3@gmail.com>
2025-10-01 04:38:17 +0900

    hammerspoon: improve

---
+++ hammerspoon/init.lua
@@ -0,0 +1,12 @@
+-- copy to ~/.hammerspoon/init.lua
+snap = require("snap")
+task = snap.task
+
+-- example
+apps = {
+	dir="finder", term="wezterm",
+	web="firefox", editor="visual studio code", kb="obsidian", git="fork",
+	hello=task.task("/bin/echo", {os.getenv("HOME") .. "HELLO"})
+}
+task.regist("option", {"term", "dir", "web", "editor", "git"})
+task.regist("option+shift", {[3]="web_alt", [4]="kb"})
